04831f84c506c986cf4a090d7f74e1c38bab6845
viscosenpai <kama.hiroki@gmail.com>
2022-01-24 00:12:26 +0900

    [fix] index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-import fs from "fs";
-import mdToPdf from "md-to-pdf";
+// index.js
+import mtp from "md-to-pdf";
 
-(async () => {
-  const pdf = await mdToPdf(
-    { path: "README.md" },
-    { dest: "README.pdf" }
-  ).catch(console.error);
-
-  if (pdf) {
-    fs.writeFileSync(pdf.filename, pdf.content);
-  }
-})();
+mtp("./README.md", { dest: "README.pdf" }).then(() => {
+  console.log("mtp Done");
+});
