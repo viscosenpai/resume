da827b888f5670a1639204f6dd3eecb4040706e1
viscosenpai <kama.hiroki@gmail.com>
2022-01-23 23:58:57 +0900

    pdf生成ワークフローの登録

---
+++ index.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import { mdToPdf } from "md-to-pdf";
+
+(async () => {
+  const pdf = await mdToPdf(
+    { path: "README.md" },
+    { dest: "README.pdf" }
+  ).catch(console.error);
+
+  if (pdf) {
+    fs.writeFileSync(pdf.filename, pdf.content);
+  }
+})();
